5839a861d5f69939b2917401f34c3f3e9be9877f
Ivan Ostapovets <124456661+Vantron777@users.noreply.github.com>
2024-02-25 13:03:53 +0100

    Update HW 12

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,7 +3,23 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export const galleryEl = document.querySelector('.gallery');
 
+if (!galleryEl) {
+  console.error('Gallery element not found on the page');
+}
+
+export async function fetchAndDisplayImages(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    imageTemplate(data);
+    smoothScroll();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
 export function imageTemplate(data) {
+  // galleryEl.innerHTML = '';
   const markup = data.hits
     .map(
       ({
@@ -18,12 +34,12 @@ export function imageTemplate(data) {
         return `<li class="gallery-item">
   <a class="gallery-link" href="${largeImageURL}">
     <img class="gallery-img" src="${webformatURL}" alt="${tags}" />
-  <div class="image-description">
-    <p><b>Likes: </b>${likes}</p>
-    <p><b>Views: </b>${views}</p>
-    <p><b>Comments: </b>${comments}</p>
-    <p><b>Downloads: </b>${downloads}</p>
-  </div>
+    <div class="image-description">
+      <p><b>Likes: </b>${likes}</p>
+      <p><b>Views: </b>${views}</p>
+      <p><b>Comments: </b>${comments}</p>
+      <p><b>Downloads: </b>${downloads}</p>
+    </div>
   </a>
 </li>`;
       }
@@ -38,4 +54,12 @@ export function imageTemplate(data) {
     captionPosition: 'bottom',
   });
   lightbox.refresh();
+}
+
+function smoothScroll() {
+  const cardHeight = galleryEl.firstElementChild.getBoundingClientRect().height;
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
